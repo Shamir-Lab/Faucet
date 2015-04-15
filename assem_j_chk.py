891a407362784531df800f2d9993b63fdc7fa62e
roye <roye.rozov@gmail.com>
2015-04-15 15:29:24 +0300

    added general buffer level retrieval

--- assem_j_chk.py
+++ assem_j_chk.py
@@ -55,6 +55,27 @@ def advance_buffer(buff, bf):
 	del buff[0] # modify original instead of copying
 
 
+def get_buffer_level(buff, j, level):
+	""" gets buffer contents from chosen level (in [0,j])
+		returns dictionary containing invariant k-j as keys
+		and k-mers including them as values - e.g., level = 0
+		contains (k-j)-mers as suffixes, level = j+1 contains 
+		them as prefixes  
+	"""
+	kmers = list(buff[level])
+	invars = {}
+	for kmer in kmers:
+		if level != 0:
+			inv = kmer[j - level : -level]
+		else:
+			inv = kmer[j - level :]
+			 
+		if inv in invars:
+			invars[inv].append(kmer)
+		else:
+			invars[inv]=[kmer]
+	return invars
+
 def get_back_suffixes(buff, j):
 	""" fetches dictionary including all k-j suffixes
 		from first level (back) of buffer as keys and the 
@@ -124,7 +145,7 @@ def load_bf_sources_sinks(filename,j,numreads):
 def clean_alt_paths_from_buff(alts, backs, fronts, buff):
 	""" given (k-j)-mers of alt paths, gets their start and end
 		k-mers to create paths to check (list returned), removes k-mers including
-		them from buffer front (modified)
+		them from buffer front (modified) and one level ahead of buffer back
 	"""
 	paths = []
 	for a in alts:
@@ -193,14 +214,19 @@ def get_candidate_false_joins(filename,bf):
 				if len(buff[-1])>1 and len(buff[0])>1:
 					# print "read no: %d, position: %d, front len %d, back len %d" % (line_no, ind, len(buff[-1]), len(buff[0]))
 					backs = get_back_suffixes(buff,j)
+					backs2 = get_buffer_level(buff,j,0)
 					fronts = get_front_prefixes(buff,j)
+					fronts2 = get_buffer_level(buff,j,j)
 					comms = (set(backs.keys())).intersection(set(fronts.keys()))
 					if ind == read_len-k:
 						break
 					next_real = kmers[ind+1][j:] # k-j suffix from next real k-mer						
 					alts = comms - set([next_real]) 
-					# print "back suffixes, back-mers: ", list(backs), backs.values()
-					# print "front prefixes, front-mers: ", list(fronts), fronts.values()
+					print "back suffixes, back-mers: ", list(backs), backs.values()
+					print "back suffixes, back-mers: ", list(backs2), backs2.values()
+					print "front prefixes, front-mers: ", list(fronts), fronts.values()
+					print "front prefixes, front-mers: ", list(fronts2), fronts2.values()
+					
 					# print "commons: ", list(comms)
 					# print "real next: ", next_real
 					# print "kmer: ", kmer
@@ -209,6 +235,13 @@ def get_candidate_false_joins(filename,bf):
 					alt_paths = clean_alt_paths_from_buff(alts, backs, fronts, buff)
 					if alt_paths:
 						cands.append(alt_paths)
+					#### to debug junctions
+					# backs = get_back_suffixes(buff,j)
+					# fronts = get_front_prefixes(buff,j)
+					# print "back suffixes, back-mers: ", list(backs), backs.values()
+					# print "front prefixes, front-mers: ", list(fronts), fronts.values()
+					####
+
 					# print "front len after cleaning: ", len(buff[-1])
 					# print "paths to check: ", alt_paths
 					# pretty_print_buffer(buff)
@@ -233,7 +266,7 @@ def check_path_for_false_joins(path, bf, reals):
 		if bools[-1]==True: # path contains some F and ends in T
 			return True 
 
-def find_real_ends(cands, hsh):
+def find_real_ends(cands, hsh, fetch_juncs = False):
 	""" given candidate sources, query hash (bf or reals) on all
 		those that have no extension on 5' end are sources
 		those that have no extension on 3' are sinks
@@ -243,57 +276,79 @@ def find_real_ends(cands, hsh):
 	sources = []
 	sinks = []
 	to_chk = []
+	juncs = []
 	for c in cands:
 		num_ext_r = 0
 		num_ext_l = 0
 		pref = c[1:]
-		suff = c[:-1]
-		# r_ext = []
-		# l_ext = []
+		suff = c[:-1]		
 		for b in bases:
 			test_kmer_r = pref+b
 			canon = min(test_kmer_r, get_rc(test_kmer_r))
 			if canon in hsh:
 				num_ext_r += 1
-				# r_ext.append(b)
 			test_kmer_l = b+suff
 			canon = min(test_kmer_l, get_rc(test_kmer_l))
 			if canon in hsh:
 				num_ext_l += 1
-				# l_ext.append(b)
+
 		if num_ext_l == 0:
 			sources.append(c)
 		elif num_ext_r == 0:
 			sinks.append(c)
+		elif fetch_juncs and (num_ext_l>1 or num_ext_r>1):
+			juncs.append(c)
 		else:
-			to_chk.append(c)#[c,r_ext,l_ext])
+			to_chk.append(c)
+
 	sources = set(sources)
 	sinks = set(sinks)
-	return (sources,sinks,to_chk)
+	juncs = set(juncs)
+	if fetch_juncs:
+		return (sources,sinks,juncs)
+	else:
+		return (sources,sinks,to_chk)
+
 
 
 ####### main ####### 
-reads_f = "/home/nasheran/rozovr/BARCODE_test_data/chr20.c10.reads.100k"
-(B,src_cnd,j_sinks,reals) = load_bf_sources_sinks(reads_f,j,100000)
+reads_f = "/home/nasheran/rozovr/BARCODE_test_data/chr20.c10.reads.head"
+(B,src_cnd,j_sinks,reals) = load_bf_sources_sinks(reads_f,j,10000)
 
-# rl_cands = get_candidate_false_joins(reads_f,reals) 
+# get and count junctions, false joins
 bf_cands = get_candidate_false_joins(reads_f,B)
 fj_cnt = 0
 br_cnt = 0
+junc_nodes = []
 for cnd_lst in bf_cands:
 	for c in cnd_lst:
 		if check_path_for_false_joins(c,B,reals):
 			fj_cnt += 1
 		else:
 			br_cnt += 1
+			junc_nodes.append(min(c[:k],get_rc(c[:k])))
+junc_nodes = set(junc_nodes)
 print "bf cands", len(bf_cands), fj_cnt, br_cnt
+print "junc_nodes", len(junc_nodes)
 
-# br_cnt = 0
-# for cnd_lst in rl_cands:
-# 	br_cnt += len(cnd_lst)
-
-# print "rl cands", len(rl_cands), br_cnt
-
+# sanity check - for debugging counts
+# count junctions using reals set
+# there should be no false joins
+rl_cands = get_candidate_false_joins(reads_f,reals) 
+fj_cnt = 0
+br_cnt = 0
+real_junc_nodes = []
+for cnd_lst in rl_cands:
+	for c in cnd_lst:
+		if check_path_for_false_joins(c,B,reals):
+			fj_cnt += 1
+		else:
+			br_cnt += 1
+			real_junc_nodes.append(min(c[:k],get_rc(c[:k])))
+real_junc_nodes = set(real_junc_nodes)
+print "real cands", len(bf_cands), fj_cnt, br_cnt
+print "real junc_nodes", len(real_junc_nodes)
+print len(junc_nodes.intersection(real_junc_nodes))
 
 # find real ends, get candidates for cheking
 sources, sinks, to_chk = find_real_ends(list(src_cnd), B)
@@ -306,6 +361,12 @@ print len(sources2), len(sinks2), len(to_discard)
 sinks |= sinks2
 sources |= sources2
 print len(sources), len(sinks)
+
+
+# sanity check - for debugging counts
+# sources, sinks, juncs = find_real_ends(list(reals), reals, fetch_juncs=True)
+# print "real sources, sinks, juncs"
+# print len(sources), len(sinks), len(juncs)
 
 
 # traverse from all sources, mark junctions visited, 
