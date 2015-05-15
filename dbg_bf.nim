aae9868d7fef6403690ebaab9fc7b9a3e3f82823
roye <roye.rozov@gmail.com>
2015-05-15 10:49:42 +0300

    added loading of sources/sinks, tested up to 1M reads

--- dbg_bf.nim
+++ dbg_bf.nim
@@ -4,6 +4,7 @@
 
 import tables
 import bloom
+import strtabs
 
 const
     bases = ['A', 'C', 'G', 'T']
@@ -28,35 +29,83 @@ proc get_rc(dna: var string): string =
     result = reverse(dna) 
     for i in 0 .. dna.high:
         result[i] = complements[result[i]]
+
+# proc get_canons()
    
-
-when isMainModule:
-    var read = "ACGTTCGTTTGACACTTCGTTTGTCGTTTGGTTCGTTGTTCGTT"
+proc load_bf_sources_sinks(fname: string, numreads: int) =
+    discard """ loads k-mers into bloom filter
+        loads to lists ends of reads as potential
+        sources and sinks (or nodes j away from sinks)
+    """
+    var sources = newStringTable() #newSeq[string]()
+    var sinks = newStringTable() #newSeq[string]()
+    var reals = newStringTable() #newSeq[string]()
     var kmers: array[0..read_len-k+1, string]
-    # get_kmers(read, k, kmers)
-    # for i,value in @kmers:
-    #     if value!=nil:
-    #         echo i, " ", value
 
-
-    echo reverse(read)
-    echo get_rc(read)
-    echo read # original isn't changed
-    
-    var bf = initialize_bloom_filter(capacity = 10_000, error_rate = fp)
-
-    # iterate over lines
-    # if line starts with ">", skip
-    var f_hand = open("chr20.c10.reads.head")
+    var bf = initialize_bloom_filter(capacity = numreads, error_rate = fp)
+    var f_hand = open(fname)
+    var line_no = 0
     for line in f_hand.lines:
+        if (line_no + 1) mod 10_000==0:
+            echo ($(line_no+1) & " read k-mers processed " & 
+                $(len(sources)) & " " & $len(sinks) & " " & $len(reals))
         get_kmers(line,k,kmers)
+        sources[kmers[0]] = nil
+        sinks[kmers[read_len-k]] = nil
         for i,value in @kmers:
             if value!=nil:
-                echo i, " ", value
+                # echo i, " ", value
+                bf.insert(value)
+                # reals only for debugging:
+                reals[value]=nil
+        inc(line_no)
 
         # continue
         # process(line)
-    i.close()
+    f_hand.close()
+
+
+# def load_bf_sources_sinks(filename,j,numreads):
+#     """ loads k-mers into bloom filter
+#         loads to lists ends of reads as potential
+#         sources and sinks (or nodes j away from sinks)
+#     """
+#     sources = []
+#     j_sinks = []
+#     reals = []
+#     B = BloomFilter(capacity = numreads * (read_len-k), error_rate=fp)
+#     line_no = 0
+#     with open(filename) as f:
+#         for line in f:
+#             if line_no+1%100000==0:
+#                 print str(line_no+1) + " read k-mers processed"
+#             read = line.rstrip()
+#             sources.append(read[:k])
+#             sources.append(get_rc(read[-k:]))
+#             j_sinks.append(read[-(k+j):-j])
+#             j_sinks.append(get_rc(read[j:j+k]))
+#             kmers = get_kmers(read,k)
+#             # insert canonical (lex-min) kmers only
+#             canons = get_canons(kmers) 
+#             B.update(canons)
+#             reals.extend(canons)
+#             line_no +=1
+#     reals = set(reals)
+#     sources = set(get_canons(sources))
+#     j_sinks = set(get_canons(j_sinks))
+#     return (B,sources,j_sinks,reals)
+
+
+when isMainModule:
+    var reads_file = "/home/nasheran/rozovr/BARCODE_test_data/chr20.c10.reads.100k"
+    load_bf_sources_sinks(reads_file, 100_000)
+    # var read = "ACGTTCGTTTGACACTTCGTTTGTCGTTTGGTTCGTTGTTCGTT"
+    # echo reverse(read)
+    # echo get_rc(read)
+    # echo read # original isn't changed
+        
+
+    
 
 # # if ACGT line get k-mers as list
 # # store first, last, last - 10 k-mers 
