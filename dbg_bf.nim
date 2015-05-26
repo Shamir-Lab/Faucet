f06512633c1aba7ad46eec149fac0126e4f124c9
roye <roye.rozov@gmail.com>
2015-05-26 09:28:20 +0300

    backup before making changes

--- dbg_bf.nim
+++ dbg_bf.nim
@@ -3,6 +3,7 @@ import bloom
 import strtabs # used often as string sets - keys are usually nil
 import strutils, sequtils
 import nimprof
+import locks
 
 const
     bases = ['A', 'C', 'G', 'T']
@@ -78,142 +79,6 @@ proc load_bf_sources_sinks(fname: string, numreads: int): auto =
     f_hand.close()
     echo($len(reals) & " real k-mers loaded")
     (bf,sources,sinks,reals)
-
-
-# proc get_empty_buff(j: int): Buff = 
-#     # newSeqWith seen at http://forum.nim-lang.org/t/1161
-#     var levels : seq[StringTableRef] = newSeqWith(j+2, newStringTable())
-#     # init with one extra empty level, that's rotated from front to back
-#     # to avoid allocations
-#     return Buff(front:0,back:0,levels:levels)
-
-# proc print_buff_info(buff: Buff) =
-#     echo("back: " & $buff.back & " front: " & $buff.front)
-#     for i in 0..len(buff.levels)-1:
-#         var x = 0
-#         var lev  = (buff.back + i) mod (j+2)
-#         echo("buff level " & $lev & ": ")
-#         for key in buff.levels[lev].keys:
-#             echo($(x+1) & ' ' & key)
-#             inc(x)
-
-# proc init_read_buff(source: string, buff: var Buff, bf: object) = 
-#     var 
-#         currs, next : StringTableRef
-#         test_kmer, canon : string
-#     buff.levels[0][source]=nil
-#     for level in 0..j:
-#         currs = buff.levels[level]
-#         next = buff.levels[level+1]
-#         for curr in currs.keys:
-#             test_kmer = curr[1..k-1]
-#             # echo(test_kmer)
-#             for b in bases:
-#                 if b == 'A': 
-#                     test_kmer.add(b)
-#                 else:
-#                     test_kmer[k-1]=b
-#                 canon = min(test_kmer, get_rc(test_kmer))
-#                 if bf.lookup(canon)==true:
-#                     next[test_kmer]=nil
-#         buff.levels[level+1] = next
-#     buff.levels[0] = newStringTable()
-#     buff.front = j+1 #(buff.front+1) mod (j+2)
-#     buff.back = 1 #(buff.back+1) mod (j+2)
-
-#         # print_buff_info(buff)
-
-# proc get_buffer_level(buff: Buff, j,level: int): TableRef[string,seq[string]] =
-#     discard """ gets buffer contents from chosen level (in [0,j])
-#         returns dictionary containing invariant k-j as keys
-#         and k-mers including them as values - e.g., level = 0
-#         contains (k-j)-mers as suffixes, level = j+1 contains 
-#         them as prefixes  
-#     """
-#     ###### nim version assumes only get front or back level ####
-    
-#     var inv: string
-#     result = newTable[string,seq[string]]()
-    
-#     for kmer in buff.levels[level].keys:
-
-#         if (level == buff.front):
-#             inv = kmer[0 .. (k-j-1)]
-#         else:
-#             inv = kmer[j .. (k-1)]
-
-#         # echo("inv in get_level " & inv)
-#         if hasKey(result, inv):
-#             result.mget(inv).add(kmer)
-#         else:
-#             result[inv] = @[kmer]
-
-# proc get_alt_paths_from_buff(comms: StringTableRef, next_real: string,
-#  backs, fronts: TableRef[string,seq[string]], buff: Buff): auto =
-#     discard """ given (k-j)-mers of alt paths, gets their start and end
-#         k-mers to create paths to check (list returned)
-#     """
-#     var
-#         pref, path : string
-#         ends = newSeq[string]()
-#     result = newSeq[string]() # newStringTable()
-#     # echo($len(comms) & ' ' & next_real)
-#     for comm in comms.keys:
-#         if comm != next_real:
-#             # echo("\n" & comm)
-#             # echo("backs")
-#             # echo(backs[comm])
-#             # echo("\n" & $backs)
-#             # echo("fronts")
-#             # echo(fronts[comm])
-#             # echo("\n" & $fronts)
-#             pref = backs[comm][0]
-#             ends = fronts[comm]
-#             for fr in ends:
-#                 path = pref & fr[k-(1+j) .. k-1]
-#                 result.add(path) # [path]=nil
-
-
-# proc clean_front(buff: var Buff, fronts:TableRef[string,seq[string]], 
-#     comms:StringTableRef) =
-#     discard """ removes all nodes starting with alt. (k-j)-mers
-#         from buffer front
-#     """
-#     var new_front = newStringTable()
-#     for fr in fronts.keys:
-#         # instead of discarding, only insert non-alts
-#         # then replace buffer front
-#         if (not hasKey(comms, fr)):
-#             for key in fronts[fr]:
-#                 new_front[key]=nil
-#     buff.levels[buff.front]=new_front
-
-# proc advance_buffer(buff: var Buff, bf: object) = 
-#     discard """ does BFS using Bloom filter bf from loaded buffer
-#         extends each front node one step, removes first level
-#     """
-#     var 
-#         fronts = newStringTable()
-#         next = newStringTable()
-#         test_kmer, canon: string
-#     fronts = buff.levels[buff.front]
-#     for node in fronts.keys:
-#         test_kmer = node[1..k-1]
-#         for b in bases:
-#             if b == 'A': 
-#                 test_kmer.add(b)
-#             else:
-#                 test_kmer[k-1]=b        
-#             canon = min(test_kmer, get_rc(test_kmer))
-#             if bf.lookup(canon)==true:
-#                 next[test_kmer]=nil
-#     buff.front = (buff.front+1) mod (j+2)
-#     # echo("new front is " & $buff.front)
-#     buff.levels[buff.front] = next
-#     # echo(next)
-#     # echo(buff.levels[buff.front])
-#     buff.back = (buff.back+1) mod (j+2)
-#     # echo("new back is " & $buff.back)
 
 
 proc load_alt_extensions(curr_real, next_real: string, 
@@ -332,9 +197,14 @@ proc get_candidate_paths(filename: string, bf: object; rc=false): auto =
 
 when isMainModule:
     var 
-        reads_file = "/vol/scratch/rozovr/chr20.c10.reads.100k"
+        reads_file = "/vol/scratch/rozovr/chr20.c10.reads.1M"
         #reads_file = "/home/nasheran/rozovr/BARCODE_test_data/chr20.c10.reads.100k"
-        (bf,sources,sinks,reals)=load_bf_sources_sinks(reads_file, 100_000)
+        (bf,sources,sinks,reals)=load_bf_sources_sinks(reads_file, 1_000_000)
         bf_cands = get_candidate_paths(reads_file, bf)
-        # bf_rc_cands  = get_candidate_paths(reads_file, bf, true)
-   
+
+    # solution with threads
+    # load BF using 1 thread
+    # find candidates with 10 threads - 
+    # each thread reads from block of reads (based on index parameter)
+    # all can query (read only from) bf
+    # each returns candidate set, take union at end
