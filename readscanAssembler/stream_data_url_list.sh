68adf4755a3cd2a28b2e984bbd02f77c85488595
ggoldsh <ggoldsh@mit.edu>
2015-08-10 04:21:52 -0400

    Added script to run on first 9 files from human genome

---
+++ readscanAssembler/stream_data_url_list.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Runs Mink with k = 27, max read length = 100, estimated kmer size = 60 million, two hash functions
+#Takes on parameter- the output file prefix
+#Streams the input file from Roye's website at http://www.tau.ac.il/~rozovr/chr20.c50.fa.gz
+
+URL_FILE=wget_urls
+READ_COMMAND=wget\ -qO-\ -i\ $URL_FILE\ \|\ bzip2\ -d\ -c\ -q
+
+eval "./mink -size_kmer 27 \
+-max_read_length 130 \
+-estimated_kmers 3000000000 \
+-read_load_file <($READ_COMMAND) \
+-file_prefix $1 \
+--two_hash \
+--just_load_bloom \
+--fastq "
+
+eval "./mink -size_kmer 27 \
+-max_read_length 130 \
+-estimated_kmers 3000000000 \
+-read_scan_file <($READ_COMMAND) \
+-bloom_file $1.bloom \
+-file_prefix $1 \
+--two_hash \
+--fastq "
