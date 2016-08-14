622569f1b94110dca8f2773f58564e69ff0e0c23
Gil Goldshlager <ggoldsh@mit.edu>
2016-08-14 02:14:22 -0700

    Reworked makefile

--- readscanAssembler/Mink.cpp
+++ readscanAssembler/Mink.cpp
@@ -11,44 +11,16 @@
 #include <vector> // for sorting_kmers
 #include <sys/time.h>
 
+#include "Mink.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 #define NNKS 4 // default minimal abundance for solidity
 #define MIN_CONTIG_SIZE (2*sizeKmer+1)
 
-float fpRate = .01;
-int j = 1;
-
-string read_load_file;
-string read_scan_file;
-string bloom_input_file;
-string junctions_input_file;
-string short_pair_filter_file;
-string long_pair_filter_file;
-
-int read_length;
-uint64_t estimated_kmers;
-bool two_hash = false;
-bool from_bloom = false;
-bool from_junctions = false;
-bool just_load = false;
-bool fastq = false;
-bool node_graph = false;
-bool paired_ends = false;
-int maxSpacerDist = 100; //max is 128, smaller --> more frequent spacers, bigger --> less frequent.  Measured in base pairs
-int64_t nb_reads;
-
-#include "../utils/Bloom.h"
-#include "../utils/Kmer.h"
-#include "ReadScanner.h"
-#include "../utils/JChecker.h"
-#include <iostream>
-#include <fstream>
-#include <string>
-
-set<kmer_type> all_kmers;
-string file_prefix;
 /*
 To run Mink, first type make in the directory to compile.
 
---
+++ readscanAssembler/Mink.h
@@ -0,0 +1,39 @@
+#ifndef MINK_MAIN
+#define MINK_MAIN
+
+#include "../utils/Bloom.h"
+#include "../utils/Kmer.h"
+#include "../utils/Junction.h"
+#include "../utils/JChecker.h"
+#include "ReadScanner.h"
+#include "ContigNode.h"
+#include "Contig.h"
+#include "ReadScanner.h"
+#include "ContigGraph.h"
+
+float fpRate = .01;
+int j = 1;
+
+string read_load_file;
+string read_scan_file;
+string bloom_input_file;
+string junctions_input_file;
+string short_pair_filter_file;
+string long_pair_filter_file;
+
+int read_length;
+uint64_t estimated_kmers;
+bool two_hash = false;
+bool from_bloom = false;
+bool from_junctions = false;
+bool just_load = false;
+bool fastq = false;
+bool node_graph = false;
+bool paired_ends = false;
+int maxSpacerDist = 100; //max is 128, smaller --> more frequent spacers, bigger --> less frequent.  Measured in base pairs
+int64_t nb_reads;
+
+set<kmer_type> all_kmers;
+string file_prefix;
+
+#endif
